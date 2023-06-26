2e624f71b0a482d78d198ddcbfefe18fc021c63f
Georgy Levin <g.phyzy@gmail.com>
2023-06-26 20:42:24 +0300

    HW #24 ipCheck Regular Expressions

--- src/telran/text/Strings.java
+++ src/telran/text/Strings.java
@@ -12,12 +12,21 @@ public static String zero_300() {
 public static String ipV4Octet() {
 	//TODO
 	//positive number from 0 to 255 and leading zeros are allowed
-	return "";
+	return "\\d\\d?|[0-1]\\d\\d|2[0-4]\\d|25[0-5]";
 }
 public static String ipV4() {
 	//TODO
 	//four ipV4 octets separated by dot 123.123.255.01
-	return "";
+	
+	String ip = concatConcat();
+	for(int i = 0;i < 3;i++) {
+		ip = ip.concat("[.]").concat(concatConcat());
+	}
+
+	return ip;
+}
+private static String concatConcat() {
+	return "(".concat(ipV4Octet()).concat(")");
 }
 
 }
